c53554dead9385cda5dea5e0dd46f0bf1deaa307
bogdan.khamelyuk <bogdan.khamelyuk@blum.com>
2025-08-20 16:59:04 +0200

    login page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { LoginForm } from './auth/login/login';
+import { SignupForm } from './auth/signup/signup';
+export const routes: Routes = [
+    { path: 'login', component: LoginForm },
+    { path: 'signup', component: SignupForm }
+];
---
+++ src/app/auth/login/login.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  TuiTextfield,
+  TuiTitle,
+  TuiButton,
+  TuiAppearance,
+  TuiIcon,
+  TuiHint,
+  TuiLabel,
+  TuiLink
+} from '@taiga-ui/core';
+import { TuiCardLarge, TuiForm, TuiHeader } from '@taiga-ui/layout';
+import { TuiTextarea, TuiPassword, TuiCheckbox } from '@taiga-ui/kit';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    TuiForm,
+    ReactiveFormsModule,
+    TuiHeader,
+    TuiCardLarge,
+    TuiTextfield,
+    TuiCheckbox,
+    TuiTitle,
+    TuiButton,
+    TuiLabel,
+    TuiHint,
+    TuiIcon,
+    TuiLink,
+    TuiPassword,
+    TuiTextarea,
+    TuiAppearance
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+  standalone: true,
+})
+export class LoginForm {
+
+  constructor(private router: Router) { }
+
+  protected readonly form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    saveLogin: new FormControl(false)
+  });
+
+  navigateToResetPassword() {
+    this.router.navigate(['/reset-password']);
+    console.log("ok")
+  }
+
+  navigateToSignUp() {
+    this.router.navigate(['/signup']);
+  }
+
+  loginWithGoogle() {
+    // Implement Google login logic here
+  }
+}
